164c2c055039fee391fd8e632ed4564ff1f0f1a9
Sergey Cherepok <chr.sergey@gmail.com>
2020-02-18 15:40:56 +0300

    add chmod.sh

---
+++ chmod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+chmod 777 /git_03/img_mount.sh
+./img_mount.sh
+
+
+
+
+
+
+
--- img_Mount.sh
+++ img_Mount.sh
@@ -4,15 +4,18 @@ image="init_img_linux-linux-4.9.69-xenomai-3.0.6-led-behaviour_linux_bbb_0_b_.1.
 
 rm /images/$image
 rm -r /img10
+umount /img11
+umount /img13
+umount /img12
+
+
+
 wget -P /images --user jenkins --password jenkins "http://192.168.102.115:8083/repository/init_image/$image"
 
 unzip /images/$image -d /img10/
 kpartx -av /img10//osimage.img
-echo "Mount osimage is Success"
 mount /dev/mapper/loop0p1 /img11/
 mount /dev/mapper/loop0p2 /img12/
-mount /img12/rescue.img /img13/
-echo "rescue.img mount img13"
 
 
 
