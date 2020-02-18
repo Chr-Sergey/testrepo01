d0e269d55011fceeb32c6b3fd6abb4f24628c9d6
Sergey Cherepok <chr.sergey@gmail.com>
2020-02-18 13:11:50 +0300

    1

--- img_Mount.sh
+++ img_Mount.sh
@@ -2,7 +2,7 @@
 
 image = init_img_linux-linux-4.9.69-xenomai-3.0.6-led-behaviour_linux_bbb_0_b_.1.0.9700.0.img.zip
 
-rm /images/$image
+#rm /images/$image
 wget -P /images --user jenkins --password jenkins http://192.168.102.115:8083/repository/$image
 umount /img10
 unzip $images -d /img10/
